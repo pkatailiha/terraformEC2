0ca0318d6db2a0158a3720d6cc4ab779ecda85a0
pkatailiha <pkatailiha5@gmail.com>
2024-11-17 22:08:33 +0530

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
     filter {
         name   = "name"
-        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240927"]
+        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-20240927"]
     }
 
     filter {
